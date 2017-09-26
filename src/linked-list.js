faf4c2987f228bfc688aadff1f6d54243c8dda7b
antashkevich <antoha_ale6@mail.ru>
2017-09-26 11:48:55 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,82 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+        this._nodes = [];
+    }
 
-    append(data) {}
+    append(data) {
+        var newNode = new Node(data,null,null);
 
-    head() {}
+        if (this.length == 0){
+            this._head = newNode;
+        }
 
-    tail() {}
+        this._tail = newNode;
+        this._nodes.push(newNode);
+        this.length++;
+        return this;
+    }
 
-    at(index) {}
+    head() {
+        if (this._head == null) {
+            return null;
+        }  
+        return this._head.data;
+    }
 
-    insertAt(index, data) {}
+    tail() {
+        if (this._tail == null) {
+            return null;
+        } 
+        return this._tail.data;
+    }
 
-    isEmpty() {}
+    at(index) {return this._nodes[index].data;}
 
-    clear() {}
+    insertAt(index, data) {
+        var newNode = new Node(data,null,null);
+        this._nodes.splice(index, 0, newNode);
+        this.length++;
+        return this;
+    }
 
-    deleteAt(index) {}
+    isEmpty() {return this.length === 0;}
 
-    reverse() {}
+    clear() {
+        this._nodes = [];
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        return this;
+    }
 
-    indexOf(data) {}
+    deleteAt(index) {
+        this._nodes.splice(index,1);
+        this.length--;
+        return this;
+    }
+
+    reverse() {
+        var temp = this._tail;
+        this._tail = this._head;
+        this._head = temp;
+        this._nodes.reverse();
+        return this;
+    }
+
+    indexOf(data) {
+        for (var i=0; i < this.length; i++){
+            if (this._nodes[i].data == data) {
+                return i;
+            } 
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
+
